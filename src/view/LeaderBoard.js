1874ac87520fc3627699db3fe01b74aa4932d21b
DESKTOP-BNR81JA\taehyun <taehyun.park@iotrust.kr>
2022-08-10 23:42:38 +0900

    템플릿 레이아웃에 component 적용

--- src/view/LeaderBoard.js
+++ src/view/LeaderBoard.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import styles from './leaderboard.module.css';
+import classNames from 'classnames/bind';
+import Card from '../components/atoms/Card';
+import { useSelector } from 'react-redux';
 
+const cx = classNames.bind(styles);
 const LeaderBoard = () => {
+  const { data, loading, error } = useSelector((state) => state.user.list);
+  
   return (
     <div>
-      <h1>리더 보드</h1>
-      <p>리더 보드 리더 보드</p>
+      <div className={cx('leader-board-header')}>
+        {/* <Link to="/board">View All</Link>  */}
+        <p>Leader Board</p>
+      </div>
+      <div className={cx('leader-board-body')}>
+        {data.map((item, key) => {
+          return <Card cardInfo={item} key={key} />
+        })}
+      </div>
     </div>
   );
 };
--- src/view/Main.js
+++ src/view/Main.js
@@ -2,29 +2,42 @@ import React, { useEffect } from 'react';
 import ScoreBoard from '../components/atoms/ScoreBoard';
 import WalkBoard from '../components/atoms/WalkBoard';
 import Card from '../components/atoms/Card';
+import styles from './main.module.css';
+import classNames from 'classnames/bind';
 
+import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { USER_REQUEST } from '../reducers/user';
+import { getUsersData } from '../reducers/user';
+
+const cx = classNames.bind(styles);
 const Main = () => {
   const dispatch = useDispatch();
-  const { list } = useSelector((state) => state.user);
-
+  const { data, loading, error } = useSelector((state) => state.user.list);
+ 
   useEffect(() => {
-    dispatch({
-      type: USER_REQUEST
-    })
+    dispatch(getUsersData())
   }, [dispatch]); 
   
-  console.log('??D?WQ?D', list)
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생!</div>;
+  if (!data) return null;
   return (
-    <>
+    <div className={cx('main-wrapper')}>
       <ScoreBoard/>
-      <WalkBoard/>
-      {list}
-      {/* {list.map((item, index) => {
-        return <Card cardInfo={item} key={index}/>
-      })} */}
-    </>
+      <div className={cx('walk-board-body')}>
+        <p className={cx('walk-board-title')}>Estimated WALK</p> 
+        <WalkBoard/>
+      </div>
+      <div className={cx('leader-board-header')}>
+        <p>Leader Board</p>
+        <Link to="/board">View All</Link> 
+      </div>
+      <div className={cx('leader-board-body')}>
+        {data.map((item, index) => {
+          return <Card cardInfo={item} key={index}/>
+        })}
+      </div>
+    </div>
   );
 };
 
