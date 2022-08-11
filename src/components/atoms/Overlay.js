21ac6e3d284998d166402345ba2e2398a3c137ac
wsdsdssas <87296669+wsdsdssas@users.noreply.github.com>
2022-08-11 19:25:24 +0900

    overlay component 생성

--- src/components/atoms/Overlay.js
+++ src/components/atoms/Overlay.js
@@ -3,7 +3,9 @@ import styles from './overlay.module.css';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
-const Overlay = (props) => {
+const Overlay = ({item}) => {
+  const {image, serialNumber, price} = item
+
   return (
     <>
       <div
@@ -11,7 +13,22 @@ const Overlay = (props) => {
         // @click="$emit('onClick')"
         // @click.self="$emit('onClick:self')"
         >
-        <slot />
+         <div className={cx('toast-wrapper')}> 
+           <div className={cx('toast-title')}>
+             <p>Profile</p>  
+           </div>
+           <div className={cx('toast-content')}>
+            <div className={cx('toast-card-img')}>
+              <img src={image} alt="img" />
+            </div>
+            <span className={cx('toast-user-name')}>
+              { serialNumber }
+            </span>     
+          </div> 
+          <div className={cx('toast-button-content')}>
+            <button className={cx('toast-button')}>Block</button>      
+          </div>
+         </div>
       </div>
     </>
   );
