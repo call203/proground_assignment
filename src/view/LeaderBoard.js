b83c3fb657796e6041db428f465691c311b759d8
wsdsdssa <taehyun.park@iotrust.kr>
2022-08-12 00:19:33 +0900

    분리된 ToastMenu 컴포넌트 적용

--- src/view/LeaderBoard.js
+++ src/view/LeaderBoard.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import styles from './leaderboard.module.css';
 import classNames from 'classnames/bind';
 import Card from '../components/atoms/Card';
-import Overlay from '../components/atoms/Overlay';
-import { useDispatch, useSelector } from 'react-redux';
+import ToastMenu from '../components/molecules/ToastMenu';
+import { useSelector } from 'react-redux';
 import { getUserData } from '../reducers/user';
 
 const cx = classNames.bind(styles);
 const LeaderBoard = () => {
-  const dispatch = useDispatch();
-  const { list, item } = useSelector((state) => state.user);
-  const { data, loading, error } = list
- 
+  const [toastClick, setToastClick] = useState(false);
+  const [item, setItem] = useState({});
+  const { data, loading, error } = useSelector((state) => state.user.list);
+
   const clickHandler = (item) => {
-    dispatch(getUserData(item))
+    setItem(item)
+    setToastClick(true)
   }
 
   const overlayToast = () => {
-    if (item.loading) return <div>로딩중...</div>;
-    if (item.error) return <div>에러 발생!</div>;
-    if (!item.data) return null; 
-    
-    return <Overlay item={item.data}/>
+    return <ToastMenu item={item} toastClick={toastClick} setToastClick={setToastClick} />
   }
 
   if (loading) return <div>로딩중...</div>;
@@ -37,13 +33,13 @@ const LeaderBoard = () => {
       <div className={cx('leader-board-body')}>
         {data.map((value, index) => {
           return (
-            <div onClick={() => clickHandler(value)}>
-              <Card cardInfo={value} number={index} key={index} />
+            <div onClick={() => clickHandler(value)} key={index}>
+              <Card cardInfo={value} number={index} />
             </div>
           )
         })}
       </div>
-      {overlayToast()}
+      { toastClick && overlayToast() }
     </div>
   );
 };
